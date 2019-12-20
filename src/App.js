27b1564e47f27f1beeb9f2dd26af234d5ac54987
Cesar Riello <czaredesign@gmail.com>
2019-12-20 07:29:15 -0300

    add: router and screens

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    tags: [],
+    tasks: [],
+    task: {
+      id: '',
+      name: '',
+      completed: false
+    }
+  }
+
+  setParentState = state => this.setState(state)
+
+  render() {
+    const value = {
+      ...this.state,
+      setParentState: this.setParentState
+    }
+
+    return (
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/" activeClassName="selected">
+                Tasks
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/edit" activeClassName="selected">
+                Task editor
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/tags" activeClassName="selected">
+                Tags
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/" render={props => <p>tasks</p>} />
+
+          <Route path="/edit" render={props => <p>task edit</p>} />
+
+          <Route path="/tags">Tags</Route>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
